dbe8aca4222c2d7bf2418b7174720302d2d3b16b
unknown <iam@matveev.website>
2024-05-31 10:24:44 +0300

    auto push

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
 api = ""
 secret = ""
+
+telegram = "token"
+channel_id = '-1002099290904'
+
+max_pos = 100 
+tp = 0.012  # Take Profit +1.2%
+sl = 0.009  # Stop Loss -0.9%
+timeframe = 240  # 15 minutes
+mode = 1  # 1 - Isolated, 0 - Cross
+leverage = 10
+qty = 5    # Amount of USDT for one order
